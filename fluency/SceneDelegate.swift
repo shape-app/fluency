679d84547b62ff6cdac85d7cb1fb4d201a893417
Haley Jiang <112786962+haleygelato@users.noreply.github.com>
2025-07-24 14:58:26 -0400

    Add pre-commit hooks and CI formatting checks (#2)

--- fluency/SceneDelegate.swift
+++ fluency/SceneDelegate.swift
@@ -1,17 +1,16 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
     var window: UIWindow?
-    
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+
         window = UIWindow(windowScene: windowScene)
-        
+
         let homeVC = UIViewController()
         homeVC.view.backgroundColor = .systemBackground
-        
+
         window?.rootViewController = homeVC
         window?.makeKeyAndVisible()
     }
